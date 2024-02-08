d0817d510d2408217b800fae02adc00b60d9b13e
Munyaneza Ishimwe Peace <peaceishimwem@gmail.com>
2024-02-08 11:16:01 +0200

    [fix]: fixed the depedency errors and updated the sdk and changed app.d.ts -> global.d.ts

--- README.md
+++
@@ -1,1 +0,0 @@
-# AgriNexa# AgrinexaFrontend
--- src/routes/TabNavigator.js
+++ src/routes/TabNavigator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Entypo, AntDesign, MaterialCommunityIcons, Feather, Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons, Feather, Ionicons } from '@expo/vector-icons';
 import { Text, View } from 'react-native';
 
 import Dashboard from '../screens/(dashboard)/Dashboard';
